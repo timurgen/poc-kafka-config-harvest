7e8ffe8ed713b38a8107a5e0b7e70be71e5a166f
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-08-21 10:52:52 +0200

    * create __clients topic if not exists

--- src/main/java/no/sysco/simplekafkaproducerapp/KafkaConfigHarverster.java
+++ src/main/java/no/sysco/simplekafkaproducerapp/KafkaConfigHarverster.java
@@ -6,12 +6,20 @@ import java.lang.reflect.Field;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.TopicExistsException;
 
 /**
  *
@@ -32,7 +40,7 @@ public class KafkaConfigHarverster {
      * @param producer KafkaProducer
      */
     public static final void harvest(final Producer producer) {
-
+        
         try {
             Field configField = producer.getClass().getDeclaredField(CONFIG_FIELD_NAME);
 
@@ -46,6 +54,11 @@ public class KafkaConfigHarverster {
                 return null;
             });
 
+            //create TOPIC_NAME topic if not exists
+            Properties userProperties = new Properties();
+            userProperties.putAll(config.originals());
+            createTopic(TOPIC_NAME, 1, (short) 1, userProperties);
+            
             //FIXME need strategy for defining klient id if not set
             String clientId = "GENERATED_CLIENT_ID";
 
@@ -71,11 +84,34 @@ public class KafkaConfigHarverster {
     private static Map<String, Object> filterData(final Map<String, Object> configMap, final String[] filter) {
         Arrays.asList(filter).forEach((item) -> {
             //producer config map may contain particulary everything, we will mask only string values
-            if (configMap.containsKey(item)) {
+            if (configMap.get(item) != null) {
                 configMap.replace(item, "XXXX-XXXX-XXXX-XXXX");
             }
         });
         return configMap;
+    }
+    
+        /**
+     * Create new topic if not exists
+     *
+     * @param name topic name
+     * @param partitions
+     * @param rf replication factor
+     * @param prop properties
+     */
+    private static void createTopic(final String name, final int partitions, final short rf, Properties prop) {
+        try (final AdminClient adminClient = KafkaAdminClient.create(prop)) {
+            try {
+                final NewTopic newTopic = new NewTopic(name, partitions, rf);
+                final CreateTopicsResult createTopicsResult = adminClient.createTopics(Collections.singleton(newTopic));
+                createTopicsResult.values().get(name).get();
+            } catch (InterruptedException | ExecutionException e) {
+                if (!(e.getCause() instanceof TopicExistsException)) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+                // TopicExistsException - Swallow this exception, just means the topic already exists.
+            }
+        }
     }
 
 }
